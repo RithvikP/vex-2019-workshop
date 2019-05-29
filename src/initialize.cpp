689affb99f03bb873f63f66e2b1c862b63eb6d1e
Rithvik P <rithvik.panch@gmail.com>
2019-05-28 18:53:20 -0700

    Add additional comments and refactor motor code

--- src/initialize.cpp
+++ src/initialize.cpp
@@ -7,10 +7,12 @@ void initialize() {}
 
 /**
  * Runs while the robot is in the disabled state of the match or field controller.
+ * In a match, this runs between the autonomous and driver control modes.
  */
 void disabled() {}
 
 /**
  * Runs after initialize(), and before autonomous when connected to the match or field controller.
+ * You can put code for autonomous selection or other initialization tasks here.
  */
 void competition_initialize() {}
--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -7,43 +7,64 @@
  * If you desire, you can talk to a volunteer about ways to improve this code to better prepare it for competitions ahead.
  */
 
-#define DRIVE_SPEED 0.75 // Changes the maximum drive speed (range between 0 and 1)
-#define ARCADE true // Controls whether the drive is tank style (false) or arcade style (true)
-#define DRIVE_THRESHOLD 8 // Controls the minimum power that can be assigned to drive motors
+const int DRIVE_SPEED = 0.75; // Changes the maximum drive speed (range between 0 and 1)
+const int ARCADE = true; // Controls whether the drive is tank style (false) or arcade style (true)
+const int DRIVE_THRESHOLD  = 8; // Controls the minimum power that can be assigned to drive motors
+
+
+// Define the main controller of the robot.
+pros::Controller master(pros::E_CONTROLLER_MASTER);
+
+// Define the robot's motors with their respective ports.
+pros::Motor left_drive (1);
+pros::Motor right_drive (2);
+pros::Motor lift (3);
+pros::Motor claw (4);
+
+/**
+ * Assign specified powers for the right and left sides of the drivetrain to the left and right motors, respectively.
+ */
+void move_drive(int right, int left) {
+	right_drive.move(right);
+	left_drive.move(left);
+}
+
+/**
+ * Assign a specified power to the lift motor.
+ */
+void move_lift(int power) {
+	lift.move(power);
+}
+
+/**
+ * Assign a specified power to the claw motor.
+ */
+void move_claw(int power) {
+	claw.move(power);
+}
 
 /**
  * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
+ * whenever driver control mode is entered when connected to a match controller.
  *
  * If no competition control is connected, this function will run immediately
  * following initialize().
  *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
+ * If the robot is disabled or communications is lost, this task will be stopped.
+ * Re-enabling the robot will restart the task, not resume it from where it left off.
  */
 void opcontrol() {
 	/*
 	 * Define variables used by different robot functionality
 	 */
 	// Drive code variables
-	int left_y;
-	int right_x;
-	int right_y;
+	int left_y;  // Left joystick Y
+	int right_x; // Right joystick X
+	int right_y; // Rithg joystic Y
 	int right_power;
 	int left_power;
-
-	// Define motors and the controller for the robot in driver control.
-	// Motors are defined locally to prevent confusion in autonomous coding.
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-
-	pros::Motor left_drive (1);
-	pros::Motor right_drive (2);
-	pros::Motor lift (3);
-	pros::Motor claw (3);
-
+	
+	// Configure the lift and claw motors to hold any position they come to rest in
 	lift.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
 	claw.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
 
@@ -53,6 +74,7 @@ void opcontrol() {
 		 * This code controls how the robot moves around the field
 		 */
 
+		// Assign the values from the controller's joysticks to variables
 		left_y = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
 		right_x = master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
 		right_y = master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
@@ -69,16 +91,17 @@ void opcontrol() {
 		}
 
 		// Choose between arcade and tank drive controls
-		if (ARCADE) {
+		// and assign correct powers for the right and left sides of the robot
+		if (ARCADE) { // if arcade mode
 			right_power = left_y - right_x;
 			left_power = left_y + right_x;
 		}
-		else {
+		else { // if tank mode
 			right_power = right_y;
 			left_power = left_y;
 		}
 
-		// Limit motor powers
+		// Limit motor powers so that they do not exceed the maximum motor possible power value
 		if (std::abs(right_power) > 127) {
 			right_power = std::copysign(127, right_power); // Copies the sign of right_power to 127
 		}
@@ -86,13 +109,12 @@ void opcontrol() {
 			left_power = std::copysign(127, left_power); // Copies the sign of left_power to 127
 		}
 
-		// Limit drivetrain speed
+		// Limit drivetrain speed based on the constant defined above
 		right_power = (int) (right_power * DRIVE_SPEED);
 		left_power = (int) (left_power * DRIVE_SPEED);
 
 		// Assign power to the motors
-		right_drive.move(right_power);
-		left_drive.move(left_power);
+		move_drive(right_power, left_power);
 
 		/*
 		 * Lift code
@@ -100,18 +122,18 @@ void opcontrol() {
 		 */
 
 		if (master.get_digital(pros::E_CONTROLLER_DIGITAL_R1) == 1) {	// Check if a button on the controller is pressed
-			lift.move(127); // Move the lift up
+			move_lift(127); // Move the lift up
 			while (master.get_digital(pros::E_CONTROLLER_DIGITAL_R1) == 1) { // Wait for the button to be released
 				pros::delay(10);
 			}
-			lift.move(0);
+			move_lift(0); // Assign no power and allow the motor to holds it's position.
 		}
 		else if (master.get_digital(pros::E_CONTROLLER_DIGITAL_R2) == 1) {
-			lift.move(-127); // Move the lift down
+			move_lift(-127); // Move the lift down
 			while (master.get_digital(pros::E_CONTROLLER_DIGITAL_R2) == 1) {
 				pros::delay(10);
 			}
-			lift.move(0);
+			move_lift(0); // Assign no power and allow the motor to holds it's position.
 		}
 
 		/*
@@ -119,23 +141,23 @@ void opcontrol() {
 		 * This code controls the claw on the robot
 		 */
 		if (master.get_digital(pros::E_CONTROLLER_DIGITAL_L1) == 1) {
-			claw.move(127); // Close the claw
+			move_claw(127); // Close the claw
 			while (master.get_digital(pros::E_CONTROLLER_DIGITAL_L1) == 1) {
 				pros::delay(10);
 			}
-			claw.move(0);
+			move_claw(0); // Assign no power and allow the motor to holds it's position.
 		}
 		else if (master.get_digital(pros::E_CONTROLLER_DIGITAL_L2) == 1) {
-			claw.move(-127); // Open the claw
+			move_claw(-127); // Open the claw
 			while (master.get_digital(pros::E_CONTROLLER_DIGITAL_L2) == 1) {
 				pros::delay(10);
 			}
-			claw.move(0);
+			move_claw(0); // Assign no power and allow the motor to holds it's position.
 		}
 		
 
-		// IMPORTANT DO NOT REMOVE
-		// This delay allows all robot related functionality to be run between iterations of this while loop.
+		// IMPORTANT: DO NOT REMOVE
+		// This delay allows all other robot related functionality to be run between iterations of this while loop.
 		pros::delay(10);
 	}
 }
